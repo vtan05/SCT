b00e2b838239fc17c755d4f776d508584885c8bd
DESKTOP-2SSVG8K\vanta <vantan2555@gmail.com>
2023-04-05 13:13:39 +0900

    Cond Prob Finished. Bayes in progress.

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -12,77 +12,70 @@ from unidecode import unidecode
 from string import punctuation, digits, whitespace, ascii_lowercase
 
 
-def drawPlot(propTable, xTickLabel, yTickLabel, title, size_scale, saveName):
-    fig, ax = plt.subplots()
-    x_size = np.size(propTable, 0)
-    y_size = np.size(propTable, 1)
-
-    x, y = np.meshgrid(np.arange(x_size), np.arange(y_size))
-
-    im = ax.scatter(
-        x=y,
-        y=x,
-        s=propTable.transpose() * size_scale,
-        c=propTable.transpose(),
-        cmap='RdYlGn',
-        marker='s'
-    )
-
-    ax.set(xticks=np.arange(y_size), yticks=np.arange(x_size),
-           xticklabels=yTickLabel, yticklabels=xTickLabel)
-    ax.set_xticks(np.arange(y_size + 1) - 0.5, minor=True)
-    ax.set_yticks(np.arange(x_size + 1) - 0.5, minor=True)
-    ax.set_xlabel('Y')
-    ax.set_ylabel('X')
-    ax.grid(which='minor')
-    ax.set_aspect('equal', 'box')
-    ax.set_xlim([-0.5, (y_size-1) + 0.5])
-    ax.set_ylim([-0.5, (x_size-1) + 0.5])
-    ax.set_ylim(ax.get_ylim()[::-1])
-
-    ax.set_title(title, loc='center')
-    fig.colorbar(im, ax=ax)
-    fig.tight_layout()
-    plt.savefig(saveName + '.png', dpi=300, bbox_inches='tight')
-    plt.show()
-
-def gen_bigram(file):
-    myFile = open(file, 'r', encoding='utf-8')
+def gen_bigram(filename):
+    myFile = open(filename, 'r', encoding='utf-8')
     file = myFile.read()
     decoded = unidecode(file.lower())
-
     bigram = list(nltk.ngrams(decoded, 2))
 
     others = punctuation + digits + whitespace
     bigram_list = []
-    for ele in bigram:
-        ele = list(ele)
-        if ele[0] in others:
-            ele[0] = '-'
-        if ele[1] in others:
-            ele[1] = '-'
-        bigram_list.append(tuple(ele))
+    for n in bigram:
+        n = list(n)
+        if n[0] in others:
+            n[0] = '-'
+        if n[1] in others:
+            n[1] = '-'
+        bigram_list.append(tuple(n))
 
-    freq = nltk.ConditionalFreqDist(bigram_list)     
+    freq = nltk.ConditionalFreqDist(bigram_list) 
 
-    conditions = list(ascii_lowercase)
-    conditions.append('-')
-
-    pd_bigram_table = np.zeros((27, 27))
+    prob_table = np.zeros((27, 27))
     for x in range(0, 27):
         for y in range(0, 27):
-            pd_bigram_table[x][y] = freq[conditions[x]][conditions[y]] / freq.N()
+            prob_table[x][y] = freq[sorted(freq.conditions())[x]][sorted(freq.conditions())[y]] / freq.N()
 
-    bigram_df = pd.DataFrame(pd_bigram_table, index=conditions, columns=conditions)
+    df = pd.DataFrame(prob_table, index=sorted(freq.conditions()), columns=sorted(freq.conditions()))
+    fig = plt.figure()
+    sns.heatmap(df, cmap='turbo', xticklabels=True, yticklabels=True)
+    plt.title("Bigram Probabilities")
+    plt.savefig(filename[:-4] + '_bigram.png', dpi=300, bbox_inches='tight')
+    plt.close(fig)
 
-    # drawPlot(pd_bigram_table, conditions, conditions,
-    #      "Probability distribution P(X=x, Y=y) for ordered pair XY", 1000,
-    #      "fig_bigram")
-    sns.heatmap(bigram_df, cmap="crest")
-    plt.show()
+    return freq, filename
+
+def bigram_prob(freq, filename):
+    prob = nltk.ConditionalProbDist(freq, nltk.MLEProbDist) 
+
+    prob_table = np.zeros((27, 27))
+    for x in range(0, 27):
+        for y in range(0, 27):
+            prob_table[x][y] = prob[sorted(prob.conditions())[y]].prob(sorted(prob.conditions())[x])
+
+    df = pd.DataFrame(prob_table, index=sorted(prob.conditions()), columns=sorted(prob.conditions()))
+    fig = plt.figure()
+    sns.heatmap(df, cmap='turbo', xticklabels=True, yticklabels=True)
+    plt.title("Bigram Conditional Probabilities P(x|y)")
+    plt.savefig(filename[:-4] + '_condprob_xy.png', dpi=300, bbox_inches='tight')
+    plt.close(fig)
+
+    prob_table = np.zeros((27, 27))
+    for x in range(0, 27):
+        for y in range(0, 27):
+            prob_table[x][y] = prob[sorted(prob.conditions())[x]].prob(sorted(prob.conditions())[y])
+
+    df = pd.DataFrame(prob_table, index=sorted(prob.conditions()), columns=sorted(prob.conditions()))
+    fig = plt.figure()
+    sns.heatmap(df, cmap='turbo', xticklabels=True, yticklabels=True)
+    plt.title("Bigram Conditional Probabilities P(y|x)")
+    plt.savefig(filename[:-4] + '_condprob_yx.png', dpi=300, bbox_inches='tight')
+    plt.close(fig)
 
 if __name__ == "__main__":
-    print('\nNoli Me Tangere (English ver.)')
-    gen_bigram('noli_eng.txt')
-    # print('\nNoli Me Tangere (Tagalog ver.)')
-    # countAlphabet('noli_tag.txt')
+    print('Processing: Noli Me Tangere (English ver.)')
+    freq, filename = gen_bigram('noli_eng.txt')
+    bigram_prob(freq, filename)
+
+    #print('Processing: Noli Me Tangere (Tagalog ver.)')
+    #freq, filename = gen_bigram('noli_tag.txt')
+    #bigram_prob(freq, filename)
