5be6bb40cf905d9ef5b4af95717667e498b3f324
LAPTOP-BA1MV7FC\Vanessa <vantan2555@gmail.com>
2023-04-16 21:23:18 +0900

    ongoing hw3

---
+++ hw3/hw3.py
@@ -0,0 +1,162 @@
+# Scientific Concepts and Thinking
+# HW 3: Codes and Bayes' Theorem
+# Submitted by Vanessa Tan 20225640
+
+import nltk
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from unidecode import unidecode
+from string import punctuation, digits, whitespace, ascii_uppercase
+
+
+count = lambda l1,l2: sum([1 for x in l1 if x in l2])
+
+def mono_prob(decoded, alphabet):
+
+    letter_freq = []
+    for letter in alphabet:
+        freq = round(decoded.count(letter) / len(decoded), 4)
+        letter_freq.append(freq)
+        #print(letter + ": " + str(freq))
+
+    others = punctuation + digits + whitespace
+    others_freq = round(count(decoded, others) / len(decoded), 4)
+    letter_freq.append(others_freq)
+    #print('_ :' + str(others_freq))
+
+    return letter_freq
+
+
+def gen_cipher(text, x, alphabet):
+
+    shift = alphabet[x:] + alphabet[:x]
+    table = text.maketrans(alphabet, shift)
+    
+    return text.translate(table)
+
+def bayes(prob, decoded):
+
+    ###### Compute Bayes' theorem ######
+
+    p_ab = prob['V'].prob('Q')
+    p_b = decoded.count('V') / len(decoded)
+    p_ba = prob['Q'].prob('V')
+    p_a = decoded.count('Q') / len(decoded)
+
+    print("Computing Bayes' Theorem")
+    print(round((p_ab * p_b), 3))
+    print(round((p_ba * p_a), 3))
+
+def bigram_prob(freq, decoded):
+
+    ###### Compute bigram conditional probabilities ######
+
+    prob = nltk.ConditionalProbDist(freq, nltk.MLEProbDist) 
+
+    prob_table = np.zeros((23, 23))
+    for x in range(0, 23):
+        for y in range(0, 23):
+            prob_table[x][y] = prob[sorted(prob.conditions())[y]].prob(sorted(prob.conditions())[x])
+
+    df = pd.DataFrame(prob_table, index=sorted(prob.conditions()), columns=sorted(prob.conditions()))
+    fig = plt.figure()
+    sns.heatmap(df, cmap='turbo', xticklabels=True, yticklabels=True)
+    plt.title("Bigram Conditional Probabilities P(x|y)")
+    plt.savefig('condprob_xy.png', dpi=300, bbox_inches='tight')
+    plt.close(fig)
+
+    prob_table = np.zeros((23, 23))
+    for x in range(0, 23):
+        for y in range(0, 23):
+            prob_table[x][y] = prob[sorted(prob.conditions())[x]].prob(sorted(prob.conditions())[y])
+
+    df = pd.DataFrame(prob_table, index=sorted(prob.conditions()), columns=sorted(prob.conditions()))
+    fig = plt.figure()
+    sns.heatmap(df, cmap='turbo', xticklabels=True, yticklabels=True)
+    plt.title("Bigram Conditional Probabilities P(y|x)")
+    plt.savefig('condprob_yx.png', dpi=300, bbox_inches='tight')
+    plt.close(fig)
+
+    bayes(prob, decoded)
+
+def gen_bigram(decoded):
+
+    ###### Generate bigram probabilities ######
+    bigram = list(nltk.ngrams(decoded, 2))
+
+    others = punctuation + digits + whitespace
+    bigram_list = []
+    i = 0
+    for n in bigram:
+        n = list(n)
+        if n[0] in others:
+            n[0] = '-'
+        if n[1] in others:
+            n[1] = '-'
+        bigram_list.append(tuple(n))
+        i += 1
+
+    freq = nltk.ConditionalFreqDist(bigram_list) 
+
+    prob_table = np.zeros((23, 23))
+    for x in range(0, 23):
+        for y in range(0, 23):
+            prob_table[x][y] = freq[sorted(freq.conditions())[x]][sorted(freq.conditions())[y]] / freq.N()
+
+    df = pd.DataFrame(prob_table, index=sorted(freq.conditions()), columns=sorted(freq.conditions()))
+    fig = plt.figure()
+    sns.heatmap(df, cmap='turbo', xticklabels=True, yticklabels=True)
+    plt.title("Bigram Probabilities")
+    plt.savefig('bigram.png', dpi=300, bbox_inches='tight')
+    plt.close(fig)
+
+    bigram_prob(freq, decoded)
+
+if __name__ == "__main__":
+    remove = ['J', 'U', 'W']
+    alphabet = list(ascii_uppercase)
+
+    for letter in remove:
+        alphabet.remove(letter)
+    alphabet = ''.join([str(elem) for elem in alphabet])
+
+    ##### Generate all 23 possible ciphers #####
+    #text = 'ROMAMOPPVGNATE'
+    #for i in range(23):
+    #    #print(i)
+    #    cipher = gen_cipher(text, i, alphabet)
+    #    #print(cipher)
+
+    ##### Generate monogram probabilities of all letters #####
+    file = 'bellogallicoincaps.txt'
+    myFile = open(file, 'r', encoding='utf-8')
+    file = myFile.read()
+    decoded = unidecode(file.upper())
+    
+    #freq = mono_prob(decoded, alphabet)
+
+    ##### Generate monogram probabilities of all letters (X = 5) #####
+    #cipher = gen_cipher(decoded, 5, alphabet)
+    #freq = mono_prob(cipher, alphabet)
+
+    ##### Generate monogram probabilities of all letters from the one-time pad #####
+    otp_file = 'xotp.txt'
+    myOtp = open(otp_file, 'r', encoding='utf-8')
+    otp_file = myOtp.read()
+    otps = list(otp_file.split("\n"))
+    
+    monoprobs = []
+    for otp in otps:
+        cipher = gen_cipher(decoded, int(otp), alphabet)
+        freq = mono_prob(cipher, alphabet)
+        monoprobs.append(freq)
+    print(np.array(monoprobs).shape)
+
+    for i in range(0,24):
+        print(alphabet[i] + ": " + str(np.mean(monoprobs[i])))
+
+    #gen_bigram(decoded)
+    
